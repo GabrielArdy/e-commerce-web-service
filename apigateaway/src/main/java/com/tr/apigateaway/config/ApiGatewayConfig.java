aba2dfb17453f1605319774f947f56dfccfa9e01
franzimato <95061641+franzimato@users.noreply.github.com>
2024-07-04 11:47:21 +0700

    Add files via upload

---
+++ apigateaway/src/main/java/com/tr/apigateaway/config/ApiGatewayConfig.java
@@ -0,0 +1,22 @@
+package com.tr.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("web_api_route", r -> r.path("/api/web/**")
+                        .uri("http://localhost:9001"))
+                .route("mobile_api_route", r -> r.path("/api/mobile/**")
+                        .uri("http://localhost:9002"))
+                .route("desktop_api_route", r -> r.path("/api/desktop/**")
+                        .uri("http://localhost:9003"))
+                .build();
+    }
+}
